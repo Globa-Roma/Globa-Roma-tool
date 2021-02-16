f6df723938058b0c588c2e70be3a2d04e137b75b
Feruzteame <59234162+Feruzteame@users.noreply.github.com>
2021-02-16 04:24:11 +0100

    delete comment out code

--- clients/src/compnent/Client.js
+++ clients/src/compnent/Client.js
@@ -1,5 +1,4 @@
 import React,{useEffect, useState} from 'react';
-//import uuid from 'uuid'
 import axios from 'axios';
 
 
@@ -50,62 +49,3 @@ export default Client;
 
 
 
-// class Blog extends Component {
-//     constructor(props){
-
-//     super(props);
-//     this.state = {
-   // posts= [
-               //fullName:"",
-    //         email:"",
-    //         telephone:"",
-    //         city:"",
-    //         interest:"",
-    //         ]
-//         
-//     }
-//   };
-
-//   componentDidMount = () => {
-//     this.getBlogPost();
-//   };
-
-//   getBlogPost = () => {
-//     axios.get('/client')
-//       .then((response) => {
-//         const data = response.data;
-//         console.log(posts.data)
-//         this.setState({ posts: data });
-//       })
-//       .catch((error) => {
-//         console.log('warnning', error);
-//       });
-//   };
-
-// displayBlogPost = (posts) => {
-
-//     if (!posts.length) return null;
-//     return posts.map((post, index) => (
-//       <div key={index} className="blog-post__display">
-//         <h3>{post.fullName}</h3>
-//         <p>{post.email}</p>
-//       </div>
-//     ));
-//   };
-
-//   render(){
-//     console.log('State: ', this.state);
-//       return(
-//             <div className="app">
-//                 <div className="blog-">
-//                 {this.displayBlogPost(this.posts)}
-//                 </div>
-//             </div>
-//             )
-//   }
-  
-
-
-
-// }
-// export default Blog;
