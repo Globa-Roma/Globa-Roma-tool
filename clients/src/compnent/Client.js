6670f5b1cfaaf1a2731c7f8cb2fde1cad2d754f4
Feruzteame <59234162+Feruzteame@users.noreply.github.com>
2021-02-16 04:24:32 +0100

    Update Client.js

--- clients/src/compnent/Client.js
+++ clients/src/compnent/Client.js
@@ -33,13 +33,7 @@ function Client(){
            </div>
             )
 
-    /// client is object not array
-            //var myObject = { 'a': 1, 'b': 2, 'c': 3 };
-            // Object.keys(myObject).map(function(key, index) {
-            //   myObject[key] *= 2;
-            // });
-            // console.log(myObject);
-            // // => { 'a': 2, 'b': 4, 'c': 6 }
+  
 }
 export default Client;
 
