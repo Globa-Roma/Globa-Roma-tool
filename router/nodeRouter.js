df4c0564914378852b0fac4eff71777afe3ef728
Feruz Teame <teamefitwi@gmail.com>
2021-04-21 22:12:04 +0200

    send email to multiple people from data base

--- router/nodeRouter.js
+++ router/nodeRouter.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const mailerRouter = express.Router();
- const Nodemailer = require('nodemailer')
+ const Nodemailer = require('nodemailer');
+const Clients = require('../modles/signinSchema')
 
 
 
 mailerRouter.post('/clients', (req, res) => {
 
-const data = req.body
-console.log(`${data.name}, ${data.email}, ${data.subject}, ${data.messages}`)
-const output = `Name: ${data.name} <br>
-                Email: ${data.email}<br><br>
-                
-                ${data.messages}`
+  const data = req.body
 
- // file
+  // file
   if (req.files === null) {
     return res.status(400).json({ msg: 'No file uploaded' });
   }
@@ -31,48 +27,42 @@ const output = `Name: ${data.name} <br>
     res.json({ fileName: file.name, filePath: `/images/${file.name}` });
   });
 
-
-  
-
-// create reusable transporter object using the default SMTP transport
-    let transporter = Nodemailer.createTransport({
-      service: 'Gmail',
-      post: 587,
-      secure: false,
-      auth:{
+  Clients.find({}, function(err, allUsers){
+  if(err){
+      console.log(err);
+  }
+  var mailList = [];
+  allUsers.forEach(function(users){
+      mailList.push(users.email);
+      return mailList;
+  });
+  var smtpTransport = Nodemailer.createTransport({
+      service: 'Gmail', 
+      auth: {
         user: 'globa.roma10',
         pass: "globaroma100%"
-      },
-      tls:{
-        rejectUnauthorized:false
       }
-    });
+  });
+  var mailOptions = {
+          to: [],
+          bcc: mailList,
+          from: `${data.email}`,
+          subject: `${data.subject}`,
+          text: `${data.messages}`,
+          attachments:[{
+            path: `${__dirname}/../clients/public/images/${file.name}`,
+          }]
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+          if(err){
+              console.log(err);
+          }
+          console.log('mail sent to ' + mailList);
+      });
+});
 
-// setup email data with unicode symbols
-   let mailOptions ={
-      from: `Globa-Aroma}`,
-      to: "globa.roma10@gmail.com", // list of recivers
-      name: `${data.name}`,
-      html: output,
-      attachments:[{
-          path: `${__dirname}/../clients/public/images/${file.name}`,
-        }]
-    };
+})
 
-// send mail with defined transport object
-    transporter.sendMail(mailOptions, (err)=>{
-        if(err){
-          return console.log(err)
-        }else{
-          console.log(`Success`)
-        }
-    })
-  
-    transporter.close()
-
-  })
+//}
 
   module.exports = mailerRouter;
-  
-
-  
